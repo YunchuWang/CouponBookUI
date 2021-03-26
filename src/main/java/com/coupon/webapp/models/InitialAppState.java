d228d90d7868ccc5167ea5333d741b2a0853a1ee
ssdd2013 <shutao2011@gmail.com>
2021-03-26 02:18:30 -0500

    NOBUG: set up webpack

--- src/main/java/com/coupon/webapp/models/InitialAppState.java
+++ src/main/java/com/coupon/webapp/models/InitialAppState.java
@@ -1,10 +1,14 @@
 package com.coupon.webapp.models;
 
 import com.coupon.webapp.annotations.ImmutableStyle;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 @ImmutableStyle
 @Value.Immutable
+@JsonSerialize(as = ImmutableInitialAppState.class)
+@JsonDeserialize(as = ImmutableInitialAppState.class)
 public interface InitialAppState {
 
     String exampleName();
--- src/main/npm/src/app.tsx
+++ src/main/npm/src/app.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// @ts-ignore
+import MyImage from './images/banner.jpg';
+// @ts-ignore
+import FavIcon from './images/favicon.ico';
+import './styles/main.scss';
 
 const App = () => {
     return (
         <div>
-            <div style={{ backgroundColor: 'red', color: 'white' }}>
+            <div className="test-style">
                 This is placeholder for main app js.
             </div>
+            <img src={MyImage} />
+            <img src={FavIcon} />
         </div>
     );
 };
